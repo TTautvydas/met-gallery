40c1d0ac402cdf30fbddb317674b245d2c420469
Tautvydas Stirbys <tautvydas.stirbys@gmail.com>
2024-03-22 16:03:52 +0200

    object endpoint and render

--- src/HomePage/HomePage.tsx
+++ src/HomePage/HomePage.tsx
@@ -1,5 +1,65 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { StyledImage } from "../styles/image.style";
 
-export default function Home_page() {
-  return <div>Home Page</div>;
-}
+const HomePage: React.FC = () => {
+  const [objectData, setObjectData] = useState<any>(null);
+  const [objectID, setObjectID] = useState<any>(null);
+
+  const fetchData = async (id: string) => {
+    try {
+      const response = await fetch(
+        `https://collectionapi.metmuseum.org/public/collection/v1/objects/${id}`
+      );
+      if (!response.ok) {
+        throw new Error("failed to fetch data");
+      }
+      const data = await response.json();
+      setObjectData(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  /*
+- Instead of object endpoint use department endpoint?
+- Need to filter out all objects that have photos before rendering
+*/
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    fetchData(objectID);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={objectID}
+          onChange={(e) => setObjectID(e.target.value)}
+          placeholder="Enter Art ID"
+        />
+        <button type="submit">Get art</button>
+      </form>
+      {objectData ? (
+        <div>
+          {objectData.primaryImage ? (
+            <div>
+              <h1>{objectData.title}</h1>
+              <StyledImage
+                src={objectData.primaryImage}
+                alt={objectData.title}
+              />
+            </div>
+          ) : (
+            <p>Photo not phound :/</p>
+          )}
+        </div>
+      ) : (
+        <p>Waiting...</p>
+      )}
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/styles/image.style.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const StyledImage = styled.img`
+  border-radius: 4px;
+  max-width: 50%;
+  height: auto;
+`;
