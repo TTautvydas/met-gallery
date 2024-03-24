2bbcecffc426d0c3794184ba4f3b53aad9446bc9
Tautvydas Stirbys <tautvydas.stirbys@gmail.com>
2024-03-24 22:09:51 +0200

    endpoints and render

--- src/HomePage/HomePage.tsx
+++ src/HomePage/HomePage.tsx
@@ -1,63 +1,60 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { StyledImage } from "../styles/image.style";
+import axios from "axios";
 
 const HomePage: React.FC = () => {
+  const [objectList, setObjectList] = useState([]);
+  const [randomID, setRandomID] = useState<number | null>(null);
+  const [primaryImage, setPrimaryImage] = useState<string | null>(null);
   const [objectData, setObjectData] = useState<any>(null);
-  const [objectID, setObjectID] = useState<any>(null);
 
-  const fetchData = async (id: string) => {
-    try {
-      const response = await fetch(
-        `https://collectionapi.metmuseum.org/public/collection/v1/objects/${id}`
-      );
-      if (!response.ok) {
-        throw new Error("failed to fetch data");
-      }
-      const data = await response.json();
-      setObjectData(data);
-    } catch (error) {
-      console.error("Error fetching data:", error);
+  useEffect(() => {
+    axios
+      .get(
+        "https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=11"
+      )
+      .then(({ data }) => {
+        setObjectList(data.objectIDs);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [objectList]);
+
+  useEffect(() => {
+    if (objectList.length > 0 && randomID === null) {
+      const randomIndex = Math.floor(Math.random() * objectList.length);
+      const randomID = objectList[randomIndex];
+      setRandomID(randomID);
     }
-  };
+  }, [objectList, randomID]);
 
-  /*
-- Instead of object endpoint use department endpoint?
-- Need to filter out all objects that have photos before rendering
-*/
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    fetchData(objectID);
-  };
+  useEffect(() => {
+    if (randomID !== null) {
+      axios
+        .get(
+          `https://collectionapi.metmuseum.org/public/collection/v1/objects/${randomID}`
+        )
+        .then(({ data }) => {
+          setPrimaryImage(data.primaryImage);
+          setObjectData(data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [randomID]);
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={objectID}
-          onChange={(e) => setObjectID(e.target.value)}
-          placeholder="Enter Art ID"
-        />
-        <button type="submit">Get art</button>
-      </form>
-      {objectData ? (
-        <div>
-          {objectData.primaryImage ? (
-            <div>
-              <h1>{objectData.title}</h1>
-              <StyledImage
-                src={objectData.primaryImage}
-                alt={objectData.title}
-              />
-            </div>
-          ) : (
-            <p>Photo not phound :/</p>
-          )}
-        </div>
-      ) : (
-        <p>Waiting...</p>
+      <h1> {randomID}</h1>
+      {objectData && (
+        <>
+          <h3>{objectData.title}</h3>
+          <h4>{objectData.artistDisplayName}</h4>
+        </>
       )}
+      {primaryImage && <StyledImage src={primaryImage} alt="Primary image" />}
     </div>
   );
 };
